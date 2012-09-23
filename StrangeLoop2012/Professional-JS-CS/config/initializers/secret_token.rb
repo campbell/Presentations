0adea38e949d16e00ee8ab4f8d85001980abcf61
Pete Campbell <pete@sumirolabs.com>
2012-09-23 12:35:46 -0400

    Resources page for Strangeloop

---
+++ StrangeLoop2012/Professional-JS-CS/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strangeloop::Application.config.secret_token = 'c16995e29e3788ef5fbaea00605c7673b8a86446594bcdb40ae028b2db99b31a040279502847140e22e76afac799dfda38f2c7f8f729ac453719233ba70e5a2d'
